b27d9e9bb5d4f0156399528f8f903496698ff222
devonrojas <devonmrojas@gmail.com>
2019-04-26 16:49:49 -0700

    motor

---
+++ ferris-wheel/src/app/modules/motor/motor-speed/motor-speed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motor-speed',
+  templateUrl: './motor-speed.component.html',
+  styleUrls: ['./motor-speed.component.scss']
+})
+export class MotorSpeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ferris-wheel/src/app/modules/motor/motor.module.ts
+++ ferris-wheel/src/app/modules/motor/motor.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MotorComponent } from './motor.component';
+import { MotorSpeedComponent } from './motor-speed/motor-speed.component';
+import { MotorDirectionComponent } from './motor-direction/motor-direction.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MotorComponent, MotorSpeedComponent, MotorDirectionComponent],
   imports: [
     CommonModule
   ]
