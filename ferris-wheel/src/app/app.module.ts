67b6188ea87694973307599d60f241351c3eb479
devonrojas <devonmrojas@gmail.com>
2019-04-26 16:59:08 -0700

    Module

--- ferris-wheel/src/app/app.module.ts
+++ ferris-wheel/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ferris-wheel/src/app/modules/dashboard/dashboard.module.ts
+++ ferris-wheel/src/app/modules/dashboard/dashboard.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MotorModule } from '../motor/motor.module';
+
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MotorModule
+  ],
+  exports: [
+    MotorModule
   ]
 })
 export class DashboardModule { }
