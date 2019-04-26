e08bafa8ec76819c85ca1d89f6a6b6105ef6aa8e
devonrojas <devonmrojas@gmail.com>
2019-04-26 16:48:30 -0700

    Imports

--- ferris-wheel/src/app/modules/dashboard/dashboard.module.ts
+++ ferris-wheel/src/app/modules/dashboard/dashboard.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MotorModule } from '../motor/motor.module';
+import { ColorPickerModule } from '../color-picker/color-picker.module';
+import { BluetoothModule } from '../bluetooth/bluetooth.module';
+
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MotorModule,
+    ColorPickerModule,
+    BluetoothModule
   ]
 })
 export class DashboardModule { }
